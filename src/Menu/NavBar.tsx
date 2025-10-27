1e2c654da06b3c476d674a7e55057b58cdae1df5
B24DCCC009 - Đặng Tuấn Anh <AnhDT.B24CC009@stu.ptit.edu.vn>
2025-10-27 15:41:14 +0700

    Update thông tin

--- src/Menu/NavBar.tsx
+++ src/Menu/NavBar.tsx
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <div className="container-navbar">  
     <nav className="navbar">
-      <Link to="/bai1">Bài 1</Link>
-      <Link to="/bai2">Bài 2</Link>
-      <Link to="/bai3">Bài 3</Link>
+      <Link to="/bai1">Bài 1: Thời tiết</Link>
+      <Link to="/bai2">Bài 2: Thông tin sinh viên</Link>
+      <Link to="/bai3">Bài 3: Đọc báo</Link>
     </nav>
     </div>
   );
